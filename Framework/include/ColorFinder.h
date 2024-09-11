7d8326ff09dde419cf40f7c3cb29659531a2081b
Yen60229 <90028411+Yen60229@users.noreply.github.com>
2024-09-11 10:41:15 +0800

    Update ColorFinder.h

--- Framework/include/ColorFinder.h
+++ Framework/include/ColorFinder.h
@@ -22,17 +22,19 @@ namespace Robot
     class ColorFinder
     {
     private:
-        Point2D m_center_point;
+        Point2D m_center_point; 
+        //儲存了檢測到的顏色區域的中心點，
+        //Point2D 是一個自定義結構或類別，應該用來表示二維平面上的座標。
 
         void Filtering(Image* img);
 	void RGBFiltering(Image* img);
 
     public:
-        int m_hue;             /* 0 ~ 360 */
-        int m_hue_tolerance;   /* 0 ~ 180 */
-        int m_min_saturation;  /* 0 ~ 100 */
-        int m_min_value;       /* 0 ~ 100 */
-        double m_min_percent;  /* 0.0 ~ 100.0 */
+        int m_hue;             /* 0 ~ 360  顏色的色相*/
+        int m_hue_tolerance;   /* 0 ~ 180  色相的容差*/
+        int m_min_saturation;  /* 0 ~ 100  飽和度*/
+        int m_min_value;       /* 0 ~ 100  顏色的明暗程度 */
+        double m_min_percent;  /* 0.0 ~ 100.0 */ //檢測到的顏色占圖像的百分比範圍
         double m_max_percent;  /* 0.0 ~ 100.0 */
 
         std::string color_section;
@@ -40,8 +42,10 @@ namespace Robot
         Image*  m_result;
 
         ColorFinder();
+	//初始化物件，設置初始的顏色範圍與參數。
         ColorFinder(int hue, int hue_tol, int min_sat, int min_val, double min_per, double max_per);
         virtual ~ColorFinder();
+	//解構函式釋放資源。
 
         void LoadINISettings(minIni* ini);
         void LoadINISettings(minIni* ini, const std::string &section);
@@ -49,6 +53,7 @@ namespace Robot
         void SaveINISettings(minIni* ini, const std::string &section);
 
         Point2D& GetPosition(Image* hsv_img);
+	//接受一個處理過的HSV影像作為參數，並返回顏色檢測到的區域的中心座標
 	Point2D& RGBGetPosition(Image* img);
     };
 }
